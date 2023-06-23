37cde18ca8dcbad555f25513f229dbb4a092ff5c
Piwriw <Piwriw@163.com>
2023-06-23 21:07:51 +0800

    [refactor]:better way to register  and  Deregister service

---
+++ user-web/initialize/consul_register.go
@@ -0,0 +1,22 @@
+package initialize
+
+import (
+	"github.com/nacos-group/nacos-sdk-go/inner/uuid"
+	"shop_api/good-web/global"
+	"shop_api/good-web/utils/register/consul"
+)
+
+func InitRegisterClient() (consul.RegistryClient, error, string) {
+	registerClient := consul.NewRegistryClient(global.AppConf.Consul.IP, global.AppConf.Consul.Port)
+	u, err := uuid.NewV4()
+	if err != nil {
+		panic("failed new  uuid:" + err.Error())
+	}
+	serverId := u.String()
+	err = registerClient.Register(global.AppConf.System.Host, global.AppConf.System.Port,
+		global.AppConf.System.Name, global.AppConf.System.Tags, serverId)
+	if err != nil {
+		return nil, err, ""
+	}
+	return registerClient, nil, serverId
+}
--- user-web/main.go
+++ user-web/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"go.uber.org/zap"
+	"os"
+	"os/signal"
 	"shop_api/user-web/global"
 	"shop_api/user-web/initialize"
 	myvalidator "shop_api/user-web/validator"
+	"syscall"
 )
 
 func main() {
@@ -30,8 +33,21 @@ func main() {
 		fmt.Printf("validator InitCustomValidator failed, err:%v\n", err)
 		return
 	}
-	zap.S().Info("启动服务器，端口:%d", global.AppConf.System.Port)
-	if err := router.Run(fmt.Sprintf("%s:%d", global.AppConf.System.Host, global.AppConf.System.Port)); err != nil {
-		zap.S().Panic(err)
+	registerClient, err, serverId := initialize.InitRegisterClient()
+	if err != nil {
+		fmt.Printf("init Consult RegisterClient  failed, err:%v\n", err)
 	}
+	zap.S().Info("启动服务器，端口:%d", global.AppConf.System.Port)
+	go func() {
+		if err := router.Run(fmt.Sprintf("%s:%d", global.AppConf.System.Host, global.AppConf.System.Port)); err != nil {
+			zap.S().Panic(err)
+		}
+	}()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	if err = registerClient.DeRegister(serverId); err != nil {
+		zap.S().Info("注销失败")
+	}
+	zap.S().Info("注销成功")
 }
